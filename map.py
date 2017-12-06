85dc03f74d235891b11323b75f44c1b52f5d6644
Akshay Bung <akshay.bung17@gmail.com>
2017-12-05 20:45:06 -0700

    Adding a point marker feature to the map

--- map.py
+++ map.py
@@ -1,3 +1,11 @@
 import folium
-map = folium.Map(location=[38.58,-99.09],zoom_start=6)
+
+
+map = folium.Map(location=[38.58,-99.09],zoom_start=6, tiles="Mapbox Bright")
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.18,-99.99], popup = "Hello Akki!", icon = folium.Icon(color='blue')))
+
+map.add_child(fg)
+
+
 map.save("Map1.html")
