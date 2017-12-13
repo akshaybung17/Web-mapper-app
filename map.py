4621e5a0ec79751765c536a088998c2de1bd41d2
Akshay Bung <akshay.bung17@gmail.com>
2017-12-12 22:27:35 -0700

    Adding a layer control on map for volcanoes and population attributes

--- map.py
+++ map.py
@@ -16,16 +16,20 @@ def color_producer(elevation):
         return 'red'
 
 map = folium.Map(location=[38.58,-99.09],zoom_start=6, tiles="Mapbox Bright")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
+for lt, ln, el in zip(lat, lon, elev):
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m",
+    fill_color=color_producer(el), color = 'grey', fill_opacity=0.7))
 
-for lt,ln,el in zip(lat,lon,elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln],radius= 6, popup = str(el)+" mts", fill_color=color_producer(el),color='grey',fill_opacity=0.7))
+fgp = folium.FeatureGroup(name="Population")
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
